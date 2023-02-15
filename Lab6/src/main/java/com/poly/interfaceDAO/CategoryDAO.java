1bc75a63a11aa9beebb45475c143280542d935e5
NguyenHan <handev2607@gmail.com>
2023-02-15 12:25:03 +0700

    Hân
    update project

--- Lab6/src/main/java/com/poly/interfaceDAO/CategoryDAO.java
+++
@@ -1,7 +0,0 @@
-package com.poly.interfaceDAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.poly.entity.Category;
-
-public interface CategoryDAO extends JpaRepository<Category, String>{}
--- Lab6/src/main/java/com/poly/interfaceDAO/OrderDAO.java
+++
@@ -1,7 +0,0 @@
-package com.poly.interfaceDAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.poly.entity.Order;
-
-public interface OrderDAO extends JpaRepository<Order, Long>{}
